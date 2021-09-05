10782f27e2f49a9ee537ade52111c311014929ee
AlbardinG <williamwillett2001@gmail.com>
2021-09-05 10:37:36 +1000

    Update ts_air_quality_logger_ext.py

--- Code/ts_air_quality_logger_ext.py
+++ Code/ts_air_quality_logger_ext.py
@@ -1,18 +1,22 @@
+from os.path import expanduser
 import json
 import time
 from sds011 import *
 import aqi
-from datetime import datetime
 import paho.mqtt.client as mqtt
+from pijuice import PiJuice
+from datetime import datetime
+
+time.sleep (120)
+
 
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
-THINGSBOARD_HOST = '10.0.0.61'
-ACCESS_TOKEN = 'x=zR]:fTZR'
-
-# Data capture and upload interval in seconds. Less interval will eventually hang the DHT22.
+THINGSBOARD_HOST = 
+ACCESS_TOKEN = 
+pijuice = PiJuice(1, 0x14)
 INTERVAL = 30
-
-sensor_data = {'pmt_2_5': 0, 'aqi_2_5': 0, 'pmt_10': 0, 'aqi_10': 0}
+sensor_data = {'pmt_2_5': 0, 'aqi_2_5': 0, 'pmt_10': 0, 'aqi_10': 0, 'batlevel': 0, 'powdraw': 0}
+print("It worked!" + str(datetime.now()))
 
 
 def get_data(n=3):
@@ -32,6 +36,18 @@ def get_data(n=3):
     return pmt_2_5, pmt_10
 
 
+def getjuice():
+    juice = pijuice.status.GetChargeLevel()
+    time.sleep(10)
+    batlevel = juice['data']
+    time.sleep(10)
+    juice = pijuice.status.GetIoCurrent()
+    time.sleep(10)
+    powdraw = juice['data']
+    time.sleep(10)
+    return batlevel, powdraw
+
+
 def conv_aqi(pmt_2_5, pmt_10):
     aqi_2_5 = aqi.to_iaqi(aqi.POLLUTANT_PM25, str(pmt_2_5))
     aqi_10 = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(pmt_10))
@@ -41,7 +57,7 @@ def conv_aqi(pmt_2_5, pmt_10):
 def save_log():
     with open("air_quality.csv", "a") as log:
         dt = datetime.now()
-        log.write("{},{},{},{},{}\n".format(dt, pmt_2_5, aqi_2_5, pmt_10, aqi_10))
+        log.write("{},{},{},{},{}\n".format(dt, pmt_2_5, aqi_2_5, pmt_10, aqi_10, batlevel))
     log.close()
 
 
@@ -61,14 +77,18 @@ try:
     while True:
         pmt_2_5, pmt_10 = get_data()
         aqi_2_5, aqi_10 = conv_aqi(pmt_2_5, pmt_10)
-        sensor_data['pmt_2_5'] = pmt_2_5
-        sensor_data['pmt_10'] = pmt_10
-        sensor_data['aqi_2_5'] = aqi_2_5
-        sensor_data['aqi_10'] = aqi_10
+        batlevel, powdraw = getjuice()
+        sensor_data['pmt_2_5'] = str(pmt_2_5)
+        sensor_data['pmt_10'] = str(pmt_10)
+        sensor_data['aqi_2_5'] = str(aqi_2_5)
+        sensor_data['aqi_10'] = str(aqi_10)
+        sensor_data['batlevel'] = str(batlevel)
+        sensor_data['powdraw'] = str(powdraw)
 
         # save log as csv
         try:
             save_log()
+            print("[INFO] Data logged")
         except:
             print("[INFO] Failure in logging data")
         time.sleep(5)
